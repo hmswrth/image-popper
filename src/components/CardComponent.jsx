33f93a67e261df1a6a563642ec903969d786f3a7
Hemanth Mudra <47498538+hmswrth@users.noreply.github.com>
2022-12-30 15:37:29 +0530

    responsive changes

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,40 +1,46 @@
-import { Card, CardContent, Dialog, DialogContent, Modal, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Dialog,
+  DialogContent,
+  Modal,
+  Typography,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import IMAGES from "../assets";
 
 function CardComponent({ id }) {
-    const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
 
-    // open the image modal and display the image
-    const handleCardClick = () => {
-        setShowModal(true)
-    }
+  // open the image modal and display the image
+  const handleCardClick = () => {
+    setShowModal(true);
+  };
 
-    const handleClose = () => {
-        setShowModal(false)
-    }
+  const handleClose = () => {
+    setShowModal(false);
+  };
 
-    useEffect(() => {
-        console.log(IMAGES)
-    },[IMAGES])
-
+  useEffect(() => {
+    console.log(IMAGES);
+  }, [IMAGES]);
 
   return (
     <>
-    <Dialog
-  open={showModal}
-  onClose={handleClose}
-  aria-labelledby="modal-modal-title"
-  aria-describedby="modal-modal-description"
-  fullWidth={true}
-        maxWidth={'lg'}
->
-<DialogContent sx={{objectFit:'contain'}}>
-<div className="image-container">
- <img src={IMAGES[id-1]} alt={`selected picture ${id}`}></img>
-</div>
-</DialogContent>
-</Dialog>
+      <Dialog
+        open={showModal}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        fullWidth={true}
+        maxWidth={"lg"}
+      >
+        <DialogContent>
+          <div className="image-container">
+            <img src={IMAGES[id - 1]} style={{height:'80vh', width:'auto'}} alt={`selected picture ${id}`}></img>
+          </div>
+        </DialogContent>
+      </Dialog>
       <Card className="image-selection-card" onClick={handleCardClick}>
         <CardContent>
           <Typography className="image-selection-text" variant="h2" sx={{}}>
