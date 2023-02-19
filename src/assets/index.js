ed1ba1ca482935c0f6625b680df8026f2e69545b
Hemanth Mudra <47498538+hmswrth@users.noreply.github.com>
2023-02-19 22:32:27 +0530

    UI update

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,25 @@
 const IMAGES = [
- require('../assets/1.jpg'),
-require('../assets/2.jpg'),
-require('../assets/3.jpg'),
+   require('../assets/1.jpg'),
+   require('../assets/2.webp'),
+   require('../assets/3.jpg'),
+   require('../assets/4.jpg'),
+   require('../assets/5.jpg'),
+   require('../assets/6.jpg'),
+   require('../assets/7.jfif'),
+   require('../assets/8.jfif'),
+   require('../assets/9.webp'),
+   require('../assets/10.jpg'),
+   require('../assets/11.jfif'),
+   require('../assets/12.jfif'),
+   require('../assets/13.jpg'),
+   require('../assets/14.jpg'),
+   require('../assets/15.png'),
+   require('../assets/16.png'),
+   require('../assets/17.jpg'),
+   require('../assets/18.jpg'),
+   require('../assets/19.jpg'),
+   require('../assets/20.jpg'),
+
 ]
 
 export default IMAGES;
--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import CardComponent from './CardComponent'
 import './Home.css'
+import logo from '../assets/logo.jpg'
+import { Typography } from '@mui/material'
 
 function HomeComponent() {
   return (
     <>
+        <div className='header'>
+        <img src={logo} alt='college logo'></img>
+        <Typography className="image-selection-text" variant="h6" sx={{}}>
+            Battle of Brains
+          </Typography>
+        </div>
         <div className='home-container'>
-            {[...Array(100)].map((_, index) => {
+            {[...Array(20)].map((_, index) => {
                 return (
                     <CardComponent id={index+1} key={index} />
                 )
